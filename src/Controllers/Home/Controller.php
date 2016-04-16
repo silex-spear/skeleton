1698b459f9cfd08c6bbd18a7f8a07dbe85ce58d3
Steven VAIDIE <steven.vaidie@gmail.com>
2016-04-16 15:50:21 +0200

    Use TwigAwareTrait instead of inject TwigEnvironnement in each controller

--- src/Controllers/Home/Controller.php
+++ src/Controllers/Home/Controller.php
@@ -6,25 +6,22 @@ use Spear\Silex\Application\Traits;
 use Symfony\Component\HttpFoundation\Response;
 use Psr\Log\LoggerAwareTrait;
 use Psr\Log\NullLogger;
+use Spear\Silex\Provider\Traits\TwigAware;
 
 class Controller
 {
     use
         Traits\RequestAware,
-        LoggerAwareTrait;
-
-    private
-        $twig;
+        LoggerAwareTrait,
+        TwigAware;
 
     public function __construct(\Twig_Environment $twig)
     {
-        $this->twig = $twig;
-
         $this->logger = new NullLogger();
     }
 
     public function homeAction()
     {
-        return $this->twig->render('home.html.twig');
+        return $this->render('home.html.twig');
     }
 }
